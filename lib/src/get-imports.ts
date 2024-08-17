741a2c859efb10055d9d0cdab267c56ee0f1e149
anubra266 <anubra266@gmail.com>
2024-08-17 08:46:59 -0500

    chore: simplify build

--- lib/src/get-imports.ts
+++ lib/src/get-imports.ts
@@ -1,27 +1,13 @@
-import * as ts from "typescript";
 import * as fs from "fs";
 
 export function getImports(filePath: string): string {
-  const fileContent = fs.readFileSync(filePath, "utf8");
-
-  // Create a SourceFile object
-  const sourceFile = ts.createSourceFile(
-    filePath,
-    fileContent,
-    ts.ScriptTarget.Latest,
-    true
-  );
-
-  let imports = "";
-
-  function visit(node: ts.Node) {
-    if (ts.isImportDeclaration(node) || ts.isImportEqualsDeclaration(node)) {
-      imports += fileContent.substring(node.pos, node.end) + "\n";
-    }
-    ts.forEachChild(node, visit);
+  try {
+    const code = fs.readFileSync(filePath, "utf8");
+    const importRegex = /^import\s+.*?(?=;|\n|$)/gm;
+    const imports = code.match(importRegex) || [];
+    return imports.join("\n");
+  } catch {
+    console.error(`Error reading file`);
+    return "";
   }
-
-  visit(sourceFile);
-
-  return imports.trim();
 }
